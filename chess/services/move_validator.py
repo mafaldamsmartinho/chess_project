a1f4225f23af1ec6139c21dce73f55f12b476a7b
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-08 15:47:11 +0530

    validate_queen_move

--- chess/services/move_validator.py
+++ chess/services/move_validator.py
@@ -126,3 +126,18 @@ def validate_bishop_move(board: Board, start: str, end: str):
     start_position = board.position_to_index(start)
     end_position = board.position_to_index(end)
     return abs(start_position[0] - end_position[0]) == abs(start_position[1] - end_position[1])
+
+
+def  validate_queen_move(board: Board, start: str, end: str):
+    start_position = board.position_to_index(start)
+    end_position = board.position_to_index(end)
+    valid_move = True
+    if abs(start_position[0] - end_position[0]) == abs(start_position[1] - end_position[1]):
+        return valid_move
+    elif abs(start_position[0] - end_position[0]) != 0 and abs(start_position[1] - end_position[1]) == 0:
+        return valid_move
+    elif abs(start_position[0] - end_position[0]) == 0 and abs(start_position[1] - end_position[1]) != 0:
+        return valid_move
+    else:
+        valid_move = False
+        return valid_move

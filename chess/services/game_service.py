04fcde8c0a7acc229567f66c106542160b13d9d0
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-09 14:27:17 +0530

    added check for dead kings

--- chess/services/game_service.py
+++ chess/services/game_service.py
@@ -4,20 +4,37 @@ from chess.services.move_validator import is_valid_move
 
 def play_move(game: Game, start: str, end: str):
 
+    found_white_king: bool = False
+    found_black_king: bool = False
+
     if is_valid_move(game.board, start, end, game.turn):
         game.board.move_piece(start, end)
         success = True
         message = 'Move played successfully'
-        next_turn = str(game.switch_turn())
+        game.switch_turn()
+        next_turn = game.turn
     else:
         success = False
         message = 'Invalid. Wrong turn or move.'
-        next_turn = str(game.turn)
+        next_turn = game.turn
+
+    for row in game.board.board:
+        if game.white_king in row:
+            found_white_king |= True
+        if game.black_king in row:
+            found_black_king |= True
+
+    if found_white_king and not found_black_king:
+        message = 'GAME OVER!!! WHITE WINS.'
+        next_turn = None
+    elif found_black_king and not found_white_king:
+        message = 'GAME OVER!!! BLACK WINS.'
+        next_turn = None
 
     play_info: dict = {
         "success": success,
         "message": message,
-        "board": game.board,
+        "board": game.board.to_dict(),
         "next_turn": next_turn
     }
     return play_info

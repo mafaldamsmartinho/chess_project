4da23893f9145e6cecfaf6a874cd95f9a8ff28bf
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-09 11:21:07 +0530

    game class instances board and fixed turn change

--- chess/models/game.py
+++ chess/models/game.py
@@ -1,15 +1,20 @@
+from chess.models.board import Board
+
 moves_history: list = []
+
 
 class Game:
 
     def __init__(self):
         self.turn = 'white'
+        self.board = Board()
+        self.board.setup_board()
 
     def switch_turn(self):
         if self.turn == 'white':
-            self.turn == 'black'
+            self.turn = 'black'
         else:
-            self.turn == 'white'
+            self.turn = 'white'
 
     def add_move(self, move_data):
         moves_history.append(move_data)

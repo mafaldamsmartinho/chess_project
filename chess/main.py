ee9716a8cf29cd3449ab4d8775285ea1ae270092
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-10 13:03:43 +0530

    improved game display

--- chess/main.py
+++ chess/main.py
@@ -8,18 +8,15 @@ while next_turn is not None:
     game.board.to_display()
     print('Next turn: ', next_turn)
     start = input('Piece to move: ')
-    if not game.board.is_valid_position(start):
+    while not game.board.is_valid_position(start):
         print(f'{start} square not valid')
-        start = input('Piece to move: ')       
+        start = input('Piece to move: ')
+
     end = input('Destination Square: ')
-    if not game.board.is_valid_position(end):
+    while not game.board.is_valid_position(end):
         print(f'{end} square not valid')
         end = input('Destination Square: ')
 
-    if start == 'exit' or end == 'exit':
-        print("Bye, see you soon.")
-        break
-
     play: dict = play_move(game, start, end)
     next_turn = play.get("next_turn")
-    print(play.get("message"))
+    print(play.get("message"), '\n')

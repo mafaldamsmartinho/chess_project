dd2b6c054d25dd281c1eb6b6876fba425a2bd80a
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-25 10:36:33 +0530

    games status and turn fixed to end games

--- chess/database/schema.sql
+++ chess/database/schema.sql
@@ -3,8 +3,8 @@ players (id SERIAL PRIMARY KEY, name TEXT NOT NULL);
 
 CREATE TABLE IF NOT EXISTS games (id SERIAL PRIMARY KEY,
 white_id INTEGER, black_id INTEGER, status TEXT NOT NULL
-CHECK ( status IN ('ongoing', 'white_win', 'black_win')),
-turn TEXT NOT NULL CHECK ( turn IN ('black', 'white')), board JSONB);
+CHECK ( status IN ('ongoing', 'white_wins', 'black_wins')),
+turn TEXT NOT NULL CHECK ( turn IN ('black', 'white', 'finished')), board JSONB);
 
 CREATE TABLE IF NOT EXISTS moves (id SERIAL PRIMARY KEY,
 game_id INTEGER, move_number INTEGER, start_square TEXT NOT NULL,

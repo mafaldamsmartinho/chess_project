e20b05dd054a892d497b4d54cf1ff7ad0cf87561
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-25 16:06:42 +0530

    removed some unused logic game service

--- chess/services/game_service.py
+++ chess/services/game_service.py
@@ -1,13 +1,10 @@
 from chess.models.game import Game
-from chess.services.move_validator import is_valid_move
+from chess.services.move_validator import is_legal_move
 
 
 def play_move(game: Game, start: str, end: str):
 
-    found_white_king: bool = False
-    found_black_king: bool = False
-
-    if is_valid_move(game.board, start, end, game.turn): # Checks if user wants to perform a valid move according with all rules.
+    if is_legal_move(game.board, start, end, game.turn): # Checks if user wants to perform a valid move according with all rules.
         game.board.move_piece(start, end)
         success = True
         message = 'Move played successfully'
@@ -18,22 +15,7 @@ def play_move(game: Game, start: str, end: str):
         message = 'Invalid. Wrong turn or move.'
         next_turn = game.turn
 
-    for row in game.board.board: # Checks row by row if both kings still exist after the move is performed.
-        if game.white_king in row:
-            found_white_king |= True
-        if game.black_king in row:
-            found_black_king |= True
-
-    if found_white_king and not found_black_king: # Checks if any of the kings was not found to define the winner.
-        message = 'GAME OVER!!! WHITE WINS.'
-        game.status = 'white_win'
-        next_turn = None
-    elif found_black_king and not found_white_king:
-        message = 'GAME OVER!!! BLACK WINS.'
-        game.status = 'black_win'
-        next_turn = None
-
-    play_info: dict = { # creates a dictionary to store play move.
+    play_info: dict = {  # creates a dictionary to store play move.
         "success": success,
         "message": message,
         "board": game.board.to_dict(),

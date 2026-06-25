daa9d4adc9072d4d926b6ece97b9f2a6eb5a3082
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-25 16:06:05 +0530

    Moved ALLOWED_POSITIONS to a public constant

--- chess/models/board.py
+++ chess/models/board.py
@@ -10,6 +10,14 @@ SQUARE_TO_INDEX: dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5,
 LAST_ROW: list = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 PIECES_SIMPLE: dict = {'pawn': 'P', 'rook': 'R', 'knight': 'H', 'bishop': 'B', 'queen': 'Q', 'king': 'K'}
 COLOUR_SIMPLE: dict = {'black': 'B', 'white': 'W'}
+ALLOWED_POSITIONS = {'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8',
+                        'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8',
+                        'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8',
+                        'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8',
+                        'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8',
+                        'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8',
+                        'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8',
+                        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'}
 
 
 class Board:
@@ -34,14 +42,6 @@ class Board:
                                                  INDEX_MAJOUR_PIECES[col])
 
     def is_valid_position(self, position: str):
-        ALLOWED_POSITIONS = {'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8',
-                             'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8',
-                             'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8',
-                             'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8',
-                             'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8',
-                             'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8',
-                             'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8',
-                             'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'}
         return position in ALLOWED_POSITIONS
 
     def position_to_index(self, position: str):

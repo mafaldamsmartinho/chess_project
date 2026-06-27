d3a66f41347ad35d9b9ca09ca11ac90e2da2c660
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-27 16:44:29 +0530

    moved play router logic into play_move function. game service function play_move is now simplified, and reusable.

--- chess/services/game_service.py
+++ chess/services/game_service.py
@@ -1,24 +1,40 @@
 from chess.models.game import Game
 from chess.services.move_validator import is_legal_move
+from chess.models.piece import Piece
+from chess.database.repositories import get_game, deserialize_board, update_game, save_move, get_moves
 
 
-def play_move(game: Game, start: str, end: str):
+def play_move(start: str, end: str, game_id: int):
+    game_data = get_game(game_id)
+    game = Game()
+    game.status = game_data[3]
+    game.turn = game_data[4]
+    game.board = deserialize_board(game_data[5])
 
-    if is_legal_move(game.board, start, end, game.turn): # Checks if user wants to perform a valid move according with all rules.
-        game.board.move_piece(start, end)
-        success = True
-        message = 'Move played successfully'
-        game.switch_turn()  # Switch player turn
-        next_turn = game.turn
+    piece: Piece = game.board.get_piece(start)
+    captured_piece: Piece = game.board.get_piece(end)
+    move_data = get_moves(game_id)
+
+    if captured_piece is not None:
+        captured_piece = f'{captured_piece.colour}_{captured_piece.type}'
+        if captured_piece == 'white_king':
+            game.status = 'black_wins'
+            game.turn = 'finished'
+            return
+        elif captured_piece == 'black_king':
+            game.status = 'white_wins'
+            game.turn = 'finished'
+            return
+
+    if not move_data:
+        move_number = 1
     else:
-        success = False
-        message = 'Invalid. Wrong turn or move.'
-        next_turn = game.turn
+        move_number = max(row[2] for row in move_data) + 1
 
-    play_info: dict = {  # creates a dictionary to store play move.
-        "success": success,
-        "message": message,
-        "board": game.board.to_dict(),
-        "next_turn": next_turn
-    }
-    return play_info
+    if is_legal_move(game, start, end): # Checks if user wants to perform a valid move according with all rules.
+        game.board.move_piece(start, end)
+        game.switch_turn()  # Switch player turn
+        save_move(game_id, move_number, start, end, f'{piece.colour}_{piece.type}', captured_piece)
+        update_game(game_id, game.turn, game.status, game.board.to_dict())
+        return {"board": game.board.to_dict()}
+    return None

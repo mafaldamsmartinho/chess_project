626164536833493586a7e2a2f3491266f641b8c0
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-28 16:50:26 +0530

    clean and comment code

--- chess/services/game_service.py
+++ chess/services/game_service.py
@@ -1,6 +1,6 @@
+from chess.models.piece import Piece
 from chess.models.game import Game
 from chess.services.move_validator import is_legal_move, is_king_in_check_mate
-from chess.models.piece import Piece
 from chess.database.repositories import get_game, deserialize_board, update_game, save_move, get_moves
 
 
@@ -10,15 +10,9 @@ def play_move(start: str, end: str, game_id: int) -> None | dict[str, dict]:
     game.status = game_data[3]
     game.turn = game_data[4]
     game.board = deserialize_board(game_data[5])
-
     piece: Piece = game.board.get_piece(start)
     captured_piece: Piece = game.board.get_piece(end)
-    move_data = get_moves(game_id)
-
-    if not move_data:
-        move_number = 1
-    else:
-        move_number = max(row[2] for row in move_data) + 1
+    move_number = next_move_number(game_id)
 
     if is_legal_move(game, start, end):  # Checks if user wants to perform a valid move according with all rules.
         game.board.move_piece(start, end)
@@ -33,3 +27,13 @@ def play_move(start: str, end: str, game_id: int) -> None | dict[str, dict]:
         update_game(game_id, game.turn, game.status, game.board.to_dict())
         return {"board": game.board.to_dict()}
     return None
+
+
+def next_move_number(game_id: int) -> int:
+    """Checks next move number"""
+    move_data = get_moves(game_id)
+    if not move_data:
+        move_number = 1
+    else:
+        move_number = max(row[2] for row in move_data) + 1
+    return move_number
--- chess/services/move_validator.py
+++ chess/services/move_validator.py
@@ -1,50 +1,52 @@
 from chess.models.board import Board, ALLOWED_POSITIONS
 from chess.models.piece import Piece
 from chess.models.game import Game
+from chess.models.chess_logger import logger
 import copy
-from fastapi import HTTPException
 
 
-def is_legal_move(game: Game, start: str, end: str):
+def is_legal_move(game: Game, start: str, end: str) -> bool:
+    """Checks if king will be in check after this move"""
     temp_game = copy.deepcopy(game)
     if not is_valid_move(temp_game.board, start, end, game.turn):
         return False
-    # Checks if king will be in check after this move
     temp_game.board.move_piece(start, end)
     if is_king_in_check(temp_game.board, game.turn):
         return False
     return True
 
 
-def is_king_in_check_mate(game: Game, current_turn: str):
-    # Check if king is in check
-    if is_king_in_check(game.board, current_turn) and is_move_possible(game):
+def is_king_in_check_mate(game: Game, current_turn: str) -> bool:
+    """Check if king is in check and no piece move is valid"""
+    if is_king_in_check(game.board, current_turn) and is_any_move_legal(game):
         return True
     return False
 
 
-def get_king_position(board: Board, current_turn: str):
-    # Check where current turn king is
+def get_king_position(board: Board, current_turn: str) -> str:
+    """Check where current turn king is returns in a5 format"""
     for el in ALLOWED_POSITIONS:
         piece = board.get_piece(el)
         if piece is not None and piece.type == 'king' and piece.colour == current_turn:
             return el
 
 
-def is_king_in_check(board: Board, king_turn: str):
+def is_king_in_check(board: Board, king_turn: str) -> bool:
+    """Checks if king turn is in check"""
     king_position = get_king_position(board, king_turn)
     next_turn = 'white'
     if king_turn == 'white':
         next_turn = 'black'
     for el in ALLOWED_POSITIONS:
         piece = board.get_piece(el)
-        if piece is not None and piece.colour == next_turn:  # Check if king is threaten
+        if piece is not None and piece.colour == next_turn:
             if is_valid_move(board, el, king_position, next_turn):
                 return True
     return False
 
 
-def is_move_possible(game: Game):
+def split_opponents(game: Game) -> list[list, list]:
+    """Splits player turn pieces into one list, and possible moves into another list"""
     start_positions: list = []
     end_positions: list = []
 
@@ -54,7 +56,12 @@ def is_move_possible(game: Game):
             start_positions.append(el)
         else:
             end_positions.append(el)
+    return [start_positions, end_positions]
 
+
+def is_any_move_legal(game: Game) -> bool:
+    """Checks if any current turn pieces can be moved"""
+    start_positions, end_positions = split_opponents(game)
     for start in start_positions:
         for end in end_positions:
             if is_legal_move(game, start, end):
@@ -62,20 +69,21 @@ def is_move_possible(game: Game):
     return False
 
 
-def is_valid_move(board: Board, start: str, end: str, current_turn: str):
+def is_valid_move(board: Board, start: str, end: str, current_turn: str) -> bool:
+    """Global check if move is valid accosding with piece rules"""
     start_sq_piece: Piece = board.get_piece(start)
     end_sq_piece: Piece = board.get_piece(end)
 
     if end_sq_piece is not None:
         if end_sq_piece.colour == current_turn:
-            print(f'{current_turn} piece own end square piece')
+            logger.info(f'{current_turn} piece own end square piece')
             return False
     if start_sq_piece is None:
-        print(f'{start} not valid. Empty start square.')
+        logger.info(f'{start} not valid. Empty start square.')
         return False
     elif start_sq_piece.colour != current_turn:
-        print(f'{start} not valid. Wrong turn.')
-        raise HTTPException(status_code=400, detail=f'{start} not valid. Wrong turn patata.')
+        logger.info(f'{start} not valid. Wrong turn.')
+        return False
 
     if start_sq_piece.type == 'pawn' and not validate_pawn_move(board, start, end):
         return False
@@ -92,12 +100,13 @@ def is_valid_move(board: Board, start: str, end: str, current_turn: str):
     if start_sq_piece.type == 'knight':
         return True
     if not is_path_clear(board, start, end):
-        print(f'Path between {start} and {end} is not clear')
+        logger.info(f'Path between {start} and {end} is not clear')
         return False
     return True
 
 
-def is_path_clear(board: Board, start: str, end: str):
+def is_path_clear(board: Board, start: str, end: str) -> bool:
+    """Check if path between start and end is clear"""
     start_position: list = board.position_to_index(start)
     end_position: list = board.position_to_index(end)
     row_diff = end_position[0] - start_position[0]
@@ -118,7 +127,8 @@ def is_path_clear(board: Board, start: str, end: str):
     return check_squares(board, start_position, end_position, row_step, col_step)
 
 
-def check_squares(board, start_position, end_position, row_step, col_step):
+def check_squares(board, start_position, end_position, row_step, col_step) -> bool:
+    """Checks if squares from start to end and specific direction are empty"""
     row = start_position[0] + row_step
     col = start_position[1] + col_step
     square = (row, col)
@@ -132,7 +142,8 @@ def check_squares(board, start_position, end_position, row_step, col_step):
     return True
 
 
-def validate_pawn_move(board: Board, start: str, end: str):
+def validate_pawn_move(board: Board, start: str, end: str) -> bool:
+    """"Check if pawn move is valid according with colour."""
     start_position = board.position_to_index(start)
     end_position = board.position_to_index(end)
     start_piece: Piece = board.get_piece(start)
@@ -156,64 +167,61 @@ def validate_pawn_move(board: Board, start: str, end: str):
             return False
 
 
-def validate_rook_move(board: Board, start: str, end: str):
+def validate_rook_move(board: Board, start: str, end: str) -> bool:
+    """Checks if rook move is valid."""
     start_position = board.position_to_index(start)
     end_position = board.position_to_index(end)
-    valid_move: bool = True
-    if abs(start_position[0] - end_position[0]) != 0 and abs(start_position[1] - end_position[1]) == 0:
-        return valid_move
-    elif abs(start_position[0] - end_position[0]) == 0 and abs(start_position[1] - end_position[1]) != 0:
-        return valid_move
+    if abs(start_position[0] - end_position[0]) != 0 and abs(start_position[1] - end_position[1]) == 0:  # Horizontal move
+        return True
+    elif abs(start_position[0] - end_position[0]) == 0 and abs(start_position[1] - end_position[1]) != 0:  # Vertical move
+        return True
     else:
-        valid_move = False
-        return valid_move
+        return False
 
 
-def validate_knight_move(board: Board, start: str, end: str):
+def validate_knight_move(board: Board, start: str, end: str) -> bool:
+    """Checks if Knigth move is valid."""
     start_position = board.position_to_index(start)
     end_position = board.position_to_index(end)
-    valid_move: bool = True
     if abs(start_position[0] - end_position[0]) == 2 and abs(start_position[1] - end_position[1]) == 1:
-        return valid_move
+        return True
     elif abs(start_position[0] - end_position[0]) == 1 and abs(start_position[1] - end_position[1]) == 2:
-        return valid_move
+        return True
     else:
-        valid_move = False
-        return valid_move
+        return False
 
 
-def validate_bishop_move(board: Board, start: str, end: str):
+def validate_bishop_move(board: Board, start: str, end: str) -> bool:
+    """Checks if bishop move is valid."""
     start_position = board.position_to_index(start)
     end_position = board.position_to_index(end)
     return abs(start_position[0] - end_position[0]) == abs(start_position[1] - end_position[1])
 
 
-def validate_queen_move(board: Board, start: str, end: str):
+def validate_queen_move(board: Board, start: str, end: str) -> bool:
+    """Checks if queen move is valid."""
     start_position = board.position_to_index(start)
     end_position = board.position_to_index(end)
-    valid_move: bool = True
     if abs(start_position[0] - end_position[0]) == abs(start_position[1] - end_position[1]):
-        return valid_move
+        return True
     elif abs(start_position[0] - end_position[0]) != 0 and abs(start_position[1] - end_position[1]) == 0:
-        return valid_move
+        return True
     elif abs(start_position[0] - end_position[0]) == 0 and abs(start_position[1] - end_position[1]) != 0:
-        return valid_move
+        return True
     else:
-        valid_move = False
-        return valid_move
+        return False
 
 
-def validate_king_move(board: Board, start: str, end: str):
+def validate_king_move(board: Board, start: str, end: str) -> bool:
+    """Checks if king move is valid."""
     start_position = board.position_to_index(start)
     end_position = board.position_to_index(end)
-    valid_move: bool = True
     if abs(start_position[0] - end_position[0]) == 1 and abs(start_position[1] - end_position[1]) == 0:
-        return valid_move
+        return True
     elif abs(start_position[0] - end_position[0]) == 1 and abs(start_position[1] - end_position[1]) == 1:
-        return valid_move
+        return True
     elif abs(start_position[0] - end_position[0]) == 0 and abs(start_position[1] - end_position[1]) == 1:
-        return valid_move
+        return True
     else:
-        valid_move = False
-        return valid_move
+        return False
 

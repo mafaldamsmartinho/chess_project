8199602e0704598c2fffd60d56713faea8506c8f
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-29 16:25:19 +0530

    bot loop

---
+++ chess/bot.py
@@ -0,0 +1,17 @@
+from chess.database.repositories import get_bot_games
+from chess.services.bot_service import bot_play_move
+from chess.models.chess_logger import logger
+from chess.api.schemas import MessageResponse
+import time
+
+bot_games = [2]
+def bot_loop():
+    while True:
+        for game_id in bot_games:
+            bot_play_move(game_id)
+            logger.info('Bot move played successfully')
+            MessageResponse(message='Bot move played successfully.')
+        time.sleep(3)
+
+if __name__ == "__main__":
+    bot_loop()

6ebd2cd2c6808e180f19f545428441615ca5ac81
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-06-30 15:18:49 +0530

    connected bot to endpoint call function

--- chess/bot_main.py
+++ chess/bot_main.py
@@ -1,17 +1,15 @@
-from chess.database.repositories import get_bot_games
-from chess.services.bot_service import bot_play_move
+from chess.bot.client import get_bot_games_call
+from chess.bot.bot_service import bot_play_move
 from chess.models.chess_logger import logger
-from chess.api.schemas import MessageResponse
 import time
 
 
 def bot_loop():
     while True:
-        bot_games = get_bot_games()
+        bot_games = get_bot_games_call()
         for game_id in bot_games:
             bot_play_move(game_id)
             logger.info('Bot move played successfully')
-            MessageResponse(message='Bot move played successfully.')
         time.sleep(2)
 
 

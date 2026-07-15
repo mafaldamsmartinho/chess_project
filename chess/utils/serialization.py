4a966c163ca38701c44b98dd367dfe27bd381ba1
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-07-15 09:53:49 +0100

    boardstate class

--- chess/utils/serialization.py
+++ chess/utils/serialization.py
@@ -1,18 +1,20 @@
-from psycopg2.extras import Json
 from chess.models.board import Board
 from chess.models.piece import Piece
 from chess.api.schemas import PieceData
 
+SerializedPiece = dict[str, str | int]
+SerializedBoard = list[list[SerializedPiece | None]]
 
-def serialize_board(board: Board) -> Json:
+
+def serialize_board(board: Board) -> SerializedBoard:
     """Converts board dict into a json"""
-    return Json(board.to_dict())
+    return board.to_dict()
 
 
-def deserialize_board(data: list[list[dict[str, str | int] | None]]) -> Board:
+def deserialize_board(boardstate: SerializedBoard) -> Board:
     """Converts json board into a board list of el None or Piece"""
     board = Board()
-    for i, row in enumerate(data):
+    for i, row in enumerate(boardstate):
         for j, el in enumerate(row):
             if el is not None:
                 piece_data = PieceData(**el)  # pydantic instantiation

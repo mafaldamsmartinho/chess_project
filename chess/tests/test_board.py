cc2aad8695734d005f77e87b84eedada4d4009bf
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-07-15 09:55:26 +0100

    pytest initiation

---
+++ chess/tests/test_board.py
@@ -0,0 +1,14 @@
+from chess.models.board import Board
+import pytest
+
+
+class TestIsValidPosition:
+
+    @pytest.mark.parametrize("position, expected",
+                             [("a2", True),
+                              ("h8", True),
+                              ("z9", False),
+                              ("a9", False),])
+    def test_returns_bool_when_valid_position(self, position, expected):
+        board = Board()
+        assert board.is_valid_position(position=position) == expected

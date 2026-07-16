416e3d10a4ff56463cea909eb20b2f4ad2e57744
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-07-16 10:36:51 +0100

    test_board init

--- chess/tests/test_board.py
+++ chess/tests/test_board.py
@@ -1,6 +1,22 @@
 import pytest
 
 from chess.models.board import Board
+# from chess.models.enums import PieceType
+
+
+class TestInitSetupBoard:
+    def test_init_board_size_when_setup(self):
+        board = Board()
+        assert len(board.board) == 8
+        assert all(len(row) == 8 for row in board.board)
+
+    # def test_correct_number_pieces_when_setup(self):
+    #     board = Board()
+    #     for row in board.board:
+    #         for el in row:
+    #             if el == Piece
+    #     assert len(board.board) == 8
+    #     assert all(el == Piece for row in board.board)
 
 
 class TestIsValidPosition:

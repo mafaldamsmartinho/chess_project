70fd829f8680628a8244255c50afc444f7da03e8
mafaldamsmartinho <mafalda.msmartinho@gmail.com>
2026-07-17 12:21:18 +0100

    tes_game, test_board, test_piece

--- chess/tests/test_piece.py
+++ chess/tests/test_piece.py
@@ -1,3 +1,17 @@
-# import pytest
+import pytest
 
-# class TestPieceInit:
+from chess.models.enums import PieceType, GameTurn
+from chess.models.piece import Piece
+
+
+class TestPieceInit:
+    def test_valid_piece(self):
+        assert Piece(colour=GameTurn("black").value, type=PieceType.KING, index=1)
+
+    def test_invalid_piece_colour(self):
+        with pytest.raises(ValueError):
+            Piece(colour="Blue", type=PieceType.ROOK, index=1)
+
+    def test_invalid_piece_type(self):
+        with pytest.raises(ValueError):
+            Piece(colour=GameTurn("black").value, type="human", index=1)
